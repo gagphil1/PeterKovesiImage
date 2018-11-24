ab6b33ac2c75f530d1445838c5a241498eacdd4c
Philippe Gagne <philippemgagne@me.com>
2018-11-24 11:10:37 -0500

    Setup function added.

---
+++ matlabfns_setup.m
@@ -0,0 +1,27 @@
+function setup_matlabfns
+    % basepath of lib
+    basedir = fileparts(mfilename('fullpath'));
+    if exist('matlabfns', 'dir')
+        libdir = 'matlabfns';
+    else
+        libdir = 'MatlabFns';
+    end
+    
+    addpath(fullfile(basedir, libdir, 'Blender'));
+    addpath(fullfile(basedir, libdir, 'Colourmaps'));
+    addpath(fullfile(basedir, libdir, 'FingerPrints'));
+    addpath(fullfile(basedir, libdir, 'FrequencyFilt'));
+    addpath(fullfile(basedir, libdir, 'Geosci'));
+    addpath(fullfile(basedir, libdir, 'GreyTrans'));
+    addpath(fullfile(basedir, libdir, 'LineSegments'));
+    addpath(fullfile(basedir, libdir, 'Match'));
+    addpath(fullfile(basedir, libdir, 'Misc'));
+    addpath(fullfile(basedir, libdir, 'PhaseCongruency'));
+    addpath(fullfile(basedir, libdir, 'Projective'));
+    addpath(fullfile(basedir, libdir, 'Robust'));
+    addpath(fullfile(basedir, libdir, 'Rotations'));
+    addpath(fullfile(basedir, libdir, 'Shapelet'));
+    addpath(fullfile(basedir, libdir, 'Shapes'));
+    addpath(fullfile(basedir, libdir, 'Spatial'));
+    
+    
